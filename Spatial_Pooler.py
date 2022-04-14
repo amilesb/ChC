da049095a3f39a377c14c75ad52a3f7c2d4e04d3
Adam Brooks <amilesb@gmail.com>
2022-04-14 13:47:42 -0400

    Completed Spatial Pooler along with all tests.

--- Spatial_Pooler.py
+++ Spatial_Pooler.py
@@ -34,6 +34,7 @@ class Spatial_Pooler:
         self.synPermActiveInc = 0.03
         self.synPermInactiveDec = 0.015
         self.stimulusThreshold = 8
+        self.permVar = 0.1
 
         self.synapses = self.initializeSynapses()
 
@@ -49,11 +50,14 @@ class Spatial_Pooler:
                                        size=self.initialNumConnected,
                                        replace=False)
             permanences = np.random.uniform(size=self.initialNumConnected,
-                                            low=self.connectedPerm-0.1,
-                                            high=self.connectedPerm+0.1)
+                                            low=self.connectedPerm-self.permVar,
+                                            high=self.connectedPerm+self.permVar)
             synapses[i] = {'index': input_indices, 'permanence': permanences,
-                           'boostScore': 1, 'activeDutyCycle': [],
-                           'overlapDutyCycle': []}
+                           'boostScore': 1, 'activeDutyCycle': [1],
+                           'overlapDutyCycle': [1]}
+
+        synapses['meanACTIVE_DC'] = [1] * self.columnCount
+        synapses['meanOVERLAP_DC'] = [1] * self.columnCount
 
         return synapses
 
@@ -91,11 +95,6 @@ class Spatial_Pooler:
 
         return subset
 
-    def retrieveBoost(self, c):
-        '''Input a mini-column index and return the homeostatic boost factor for
-        that mini-column.'''
-        pass
-
     def computeWinningCOlumns(self, overlapScore, stimulusT=8, numW=40):  # stimulusT = self.stimulusThreshold; numW = self.numActiveColumnsPerInhArea
         '''Inputs a list of each mini-column's overlap with the input space and
         ranks each mini-column.  Ties are broken pseudorandomly and a list with
@@ -108,36 +107,56 @@ class Spatial_Pooler:
 
         return winningColumnsInd
 
-    def updateSynapseParameters(self, winningColumnsInd, currentInput):
+    def updateSynapseParameters(self, winningColumnsInd, overlapScore, currentInput):
         '''Inputs a list of winning mini-column indices along with the current
         input as a binary array and updates the permancence values for the
-        winning mini-columns only.  In addition, homeostatic boosting is
+        winning mini-columns only.  In the second loop, homeostatic boosting is
         implemented by reviewing how active a mini-column is relative to its
         neighbors and boosting up or down to normalize all mini-columns towards
-        the same activity level.'''
+        the same activity level.  Additionally, a list of overlap scores for all
+        mini-columns is used to evaluate how often each mini-column has
+        sufficient overlap with the input relative to its neighbors.  If any
+        mini-columns fall below a minimum overlap duty cycle, all that
+        mini-column's permanences are increased (i.e. search for new inputs).
+
+        Note, the boost score is updated serially while ideally (in neuro it is
+        almost certainly implemented in parallel via physical connections i.e.
+        hardware).  As a result, each boost score incorporates the duty cycle
+        from all minicolumns with index less than current from most recent time-
+        stamp and all minicolumns with index greater from time-stamp minus 1.
+        This translates to each minicolumn seeing a slightly different mean
+        active duty cycle.  However, as the duty cycle represents a running
+        average of previous 1000 timestamps, this slight difference is
+        negligible and an implementation detail only.'''
 
         for c in winningColumnsInd:
-            potentialSynapses = synapses[c]['index']
-            for s in potentialSynapses:
+            potentialSynapses = self.synapses[c]['index']
+            for idx, s in enumerate(potentialSynapses):
                 if currentInput[s] == 1:
-                    synapses[c]['permanence'][s] += self.synPermActiveInc
-                    synapses[c]['permanence'][s] = min(1.0, synapses[c]['permanence'][s])
+                    self.synapses[c]['permanence'][idx] += self.synPermActiveInc
+                    self.synapses[c]['permanence'][idx] = min(1.0, self.synapses[c]['permanence'][idx])
                 else:
-                    synapses[c]['permanence'][s] += self.synPermInactiveDec
-                    synapses[c]['permanence'][s] = max(0.0, synapses[c]['permanence'][s])
+                    self.synapses[c]['permanence'][idx] += self.synPermInactiveDec
+                    self.synapses[c]['permanence'][idx] = max(0.0, self.synapses[c]['permanence'][idx])
 
         for c in range(self.columnCount):
             self.updateActiveDutyCycle(c, winningColumnsInd)
-
-
-            # overlapDutyCycle is sliding avg how oftec c has had overlap > stimulusThreshold over last 1000 iterations
+            activeDC_c = self.calcActiveDutyCycle(c)
+            meanACTIVE_DC = self.updateAndCalcMeanDutyCycle(c, activeDC_c, type='active')
+            boostStrength = np.exp(-(activeDC_c-meanACTIVE_DC))
+            self.synapses[c]['boostScore'] = (self.synapses[c]['boostScore']*boostStrength)
+            overlapDC_c = self.updateOverlapDutyCycle(c, overlapScore[c])
+            meanOVERLAP_DC = self.updateAndCalcMeanDutyCycle(c, overlapDC_c, type='overlap')
+            minOverlapDC = self.calcMinOverlapDutyCycle(meanOVERLAP_DC)
+            if overlapDC_c < minOverlapDC:
+                self.synapses[c]['permanence'] += self.permVar
 
     def updateActiveDutyCycle(self, c, winningColumnsInd, n_iterations=1000):
         '''Input a mini-column index, a list of the winning columns, and update
         its active duty cycle based on a moving average of how often that
         column has been active after inhibition over the last n iterations.'''
 
-        activeDutyCycleList = synapses[c]['activeDutyCycle']
+        activeDutyCycleList = self.synapses[c]['activeDutyCycle']
         if c in winningColumnsInd:
             activeDutyCycleList.append(1)
         else:
@@ -146,7 +165,67 @@ class Spatial_Pooler:
         if len(activeDutyCycleList) > n_iterations:
             del activeDutyCycleList[0]
 
+        self.synapses[c].update({'activeDutyCycle': activeDutyCycleList})
+
+    def calcActiveDutyCycle(self, c):
+        '''Input a mini-column index and compute the active duty cycle.  Return
+        value as a scalar.'''
+
+        activeDutyCycleList = self.synapses[c]['activeDutyCycle']
+
         return sum(activeDutyCycleList)/len(activeDutyCycleList)
+
+    def updateAndCalcMeanDutyCycle(self, c, dutyCycle_c, type):
+        '''The mean duty cycle is initialized to a list of length
+        self.columnCount with '1' as each value because both the active and
+        overlap duty cycles for each synapse are also initialized to 1.  This
+        function takes the new duty cycle for mini-column c and replaces this
+        value in the respective list.
+        It then calculates the mean duty cycle and returns this value.  (Note,
+        this function assumes global inhibition).'''
+
+        if type == 'active':
+            label = 'meanACTIVE_DC'
+        else:
+            label = 'meanOVERLAP_DC'
+
+        self.synapses[label][c] = dutyCycle_c
+
+        return sum(self.synapses[label])/len(self.synapses[label])
+
+    def updateOverlapDutyCycle(self, c, overlap, n_iterations=1000):
+        '''The overlap duty cycle measures how often a mini-column c has had
+        overlap (active connected synapses) > stimulusThreshold over the last
+        1000 iterations.  Function inputs a mini-column index c, and scalar
+        overlap value and outputs a scalar overlap duty cycle along with
+        updating the synapse dictionary to maintain a running average.'''
+
+        overlapDutyCycleList = self.synapses[c]['overlapDutyCycle']
+        if overlap > self.stimulusThreshold:
+            overlapDutyCycleList.append(1)
+        else:
+            overlapDutyCycleList.append(0)
+
+        if len(overlapDutyCycleList) > n_iterations:
+            del overlapDutyCycleList[0]
+
+        self.synapses[c].update({'overlapDutyCycle': overlapDutyCycleList})
+
+        return sum(overlapDutyCycleList)/len(overlapDutyCycleList)
+
+    def calcMinOverlapDutyCycle(self, meanOVERLAP_DC):
+        '''Calculate the minimum overlap duty cycle based on assumed Poisson
+        statistics and a goal for each column's overlap duty cycle to be within
+        2 standard deviations of the mean.   meanOVERLAP_DC is a scalar value
+        between 0 and 1 representing the percentage overlap for all
+        mini-columns.  Returns a scalar value between 0 and 1.'''
+
+        counts_avg = meanOVERLAP_DC*100
+
+        return (counts_avg - (2*np.sqrt(counts_avg)) ) / 100
+
+
+
 
 if __name__ == '__main__':
     pass
--- test_Spatial_Pooler.py
+++ test_Spatial_Pooler.py
@@ -15,6 +15,7 @@ class TestEncoder(unittest.TestCase):
     def setUp(self):
         self.lengthEncoding = 1200
         self.sp = Spatial_Pooler(lengthEncoding=self.lengthEncoding)
+        self.sp2 = Spatial_Pooler(lengthEncoding=self.lengthEncoding)
         perms = np.ones(800)
         perms[0:400] = 0.15
         perms[401:800] = 0.25
@@ -22,6 +23,14 @@ class TestEncoder(unittest.TestCase):
                                   'boostScore': 1}
         self.currentInput = np.zeros(800)
         self.currentInput[500:700] = 1
+
+        self.winningColumnsInd = [0, 1]
+
+        self.currentInputA = np.ones(2048)
+        self.currentInputB = np.zeros(2048)
+
+        self.overlapScoreA = self.sp.computeOverlap(self.currentInputA)
+        self.overlapScoreB = self.sp.computeOverlap(self.currentInputB)
 
     def testInitializeSynapses(self):
         synapseDict = self.sp.synapses[0]
@@ -62,11 +71,132 @@ class TestEncoder(unittest.TestCase):
         assert len(smallWin) == 2
         assert 48 and 49 in smallWin
 
-    def testUpdateSynapseParameters(self):
-        pass
+    def testUpdateSynapseParameters_first_for_loop(self):
+        currentSynapsePermsA = self.sp.synapses[0]['permanence']
+        currentSynapsePermsB = self.sp.synapses[1]['permanence']
+        currentSynapsePermsC = self.sp.synapses[2]['permanence']
+
+        self.sp.updateSynapseParameters(self.winningColumnsInd, self.overlapScoreA, self.currentInputA)
+        self.sp.updateSynapseParameters(self.winningColumnsInd, self.overlapScoreB, self.currentInputA)
+
+        # assertiions for first for loop
+        assert (self.sp.synapses[0]['permanence'].all() ==
+                (currentSynapsePermsA + self.sp.synPermActiveInc).all()
+               )
+        assert (self.sp.synapses[1]['permanence'].all() ==
+                (currentSynapsePermsB - self.sp.synPermInactiveDec).all()
+               )
+        assert self.sp.synapses[2]['permanence'].all() == currentSynapsePermsC.all() # other columns not touched
+
+
+    def testUpdateSynapseParameters_second_for_loop(self):
+
+        # first 1024 columns are selected as winners 2nd 1024 not.  Overlap and Input are set to encompass every mini-column.
+        winningColumnsIdx = [i for i in range(1024)]
+        self.sp2.updateSynapseParameters(winningColumnsIdx, self.overlapScoreA, self.currentInputA)
+
+
+        activeDC_c = self.sp2.calcActiveDutyCycle(0)
+        overlapDC_c = self.sp2.updateOverlapDutyCycle(0, self.overlapScoreA[0])
+
+        assert activeDC_c == 1  # first mini-column is winner so has active duty cycle == 1
+        assert overlapDC_c == 1
+
+        activeDC_c1025 = 0.5
+        overlapDC_c1025 = 0
+        for i in range(2048):
+            if i < 1024:
+                meanACTIVE_DC = self.sp2.updateAndCalcMeanDutyCycle(i, activeDC_c, type='active')
+                meanOVERLAP_DC = self.sp2.updateAndCalcMeanDutyCycle(i, overlapDC_c, type='overlap')
+            else:
+                meanACTIVE_DC = self.sp2.updateAndCalcMeanDutyCycle(i, activeDC_c1025, type='active')
+                meanOVERLAP_DC = self.sp2.updateAndCalcMeanDutyCycle(i, overlapDC_c1025, type='overlap')
+
+            assert meanACTIVE_DC == 0.75 # mean value is same for all mini-columns.  Also, 1st half of the mini-columns selected are active 100% ([1, 1]) and half are active 50% ([1, 0]).
+        assert meanOVERLAP_DC == 0.5 # similar test as for active DC but in this case done after all mini-columns updated 1 timestamp
+
+        # Boost scores are updating correctly
+        assert self.sp2.synapses[2047]['boostScore'] == np.exp(0.25)
+
+        self.sp2.updateSynapseParameters(winningColumnsIdx, self.overlapScoreA, self.currentInputA)
+
+        assert self.sp2.synapses[0]['boostScore'] == np.exp(-0.25)
+        assert abs(self.sp2.synapses[2047]['boostScore'] - np.exp(1/3)*np.exp(0.25)) < 1e-5
+
+
+        old = self.sp2.synapses[1]['permanence'].copy()
+        self.sp2.updateSynapseParameters([], self.overlapScoreB, self.currentInputB)
+        new = self.sp2.synapses[1]['permanence']
+        test_old = list(self.sp2.permVar+old)
+        for i in range(len(new)):
+            assert new[i]-test_old[i] == 0
+
 
     def testUpdateActiveDutyCycle(self):
-        pass
+
+        self.sp.updateActiveDutyCycle(c=0, winningColumnsInd = [0])
+
+        assert self.sp.synapses[0]['activeDutyCycle'] == [1, 1]
+
+        self.sp.updateActiveDutyCycle(c=1, winningColumnsInd = [0])
+
+        assert self.sp.synapses[1]['activeDutyCycle'] == [1, 0]
+
+        testCycle = [i%2 for i in range(1000)]
+
+        for i in range(1005):
+            if i%2 == 0:
+                self.sp.updateActiveDutyCycle(c=0, winningColumnsInd = [0])
+            else:
+                self.sp.updateActiveDutyCycle(c=0, winningColumnsInd = [1])
+
+        assert self.sp.synapses[0]['activeDutyCycle'] == testCycle
+
+    def testCalcActiveDutyCycle(self):
+        activeDutyCycle = self.sp.calcActiveDutyCycle(0)
+
+        assert activeDutyCycle == 1
+
+        for i in range(1005):
+            if i%2 == 0:
+                self.sp.updateActiveDutyCycle(c=0, winningColumnsInd = [0])
+            else:
+                self.sp.updateActiveDutyCycle(c=0, winningColumnsInd = [1])
+
+        activeDutyCycle = self.sp.calcActiveDutyCycle(0)
+
+        assert activeDutyCycle == 0.5
+
+    def testUpdateAndCalcMeanDutyCycle(self):
+        meanADC = self.sp.updateAndCalcMeanDutyCycle(c=0, dutyCycle_c=1, type='active')
+
+        assert meanADC == 1.0
+
+        for i in range(1024):
+            meanADC = self.sp.updateAndCalcMeanDutyCycle(c=i, dutyCycle_c=0, type='active')
+
+        assert meanADC == 0.5
+
+    def testUpdateOverlapDutyCycle(self):
+        overlapDutyCycle = self.sp.updateOverlapDutyCycle(0, 10)
+
+        assert overlapDutyCycle == 1
+
+        for i in range(1005):
+            if i%2 == 0:
+                _ = self.sp.updateOverlapDutyCycle(c=0, overlap = 0)
+            else:
+                _ = self.sp.updateOverlapDutyCycle(c=0, overlap = 10)
+
+        overlapDutyCycle = self.sp.updateOverlapDutyCycle(0, 10)
+
+        assert overlapDutyCycle == 0.5
+
+    def testCalcMinOverlapDutyCycle(self):
+        minOverlapDC = self.sp.calcMinOverlapDutyCycle(meanOVERLAP_DC=0.5)
+        poisson50 = (50-2*np.sqrt(50))/100
+
+        assert minOverlapDC == poisson50
 
 
 if __name__ == '__main__':
